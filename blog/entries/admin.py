5635ce5fee4bbb2332f8fc0d9b4a7c50dd7ede25
Yatri Trivedi <yatri.testing@gmail.com>
2016-09-07 01:53:04 +0000

    first breakpoint

--- blog/entries/admin.py
+++ blog/entries/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from entries.models import Blog, Author, Entry
-
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
@@ -18,3 +20,14 @@ class AuthorAdmin(admin.ModelAdmin):
 class EntryAdmin(admin.ModelAdmin):
     list_display = ('id', 'blog', 'headline', 'number_comments', 'scoring')
     list_filter = ('blog',)
+    actions = ['reset_scores', 'change_blog']
+    
+    def reset_scores(self, request, queryset):
+        queryset.update(scoring=0.00)
+    
+    def change_blog(self, request, queryset):
+        selected = request.POST.getlist(admin.ACTION_CHECKBOX_NAME)
+        ct = ContentType.objects.get_for_model(queryset.model)
+        return render(request, "admin/move_entry.html")
+    
+    
